2c01b0784da277250404ad11b0a17c9cb17ce719
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2025-02-13 18:24:28 +0900

    pyserial がないぞっていうエラーが nixpkgs nixos-24.11 では解消されてる

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # spell-checker:words numtide pkgs stdenv
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
